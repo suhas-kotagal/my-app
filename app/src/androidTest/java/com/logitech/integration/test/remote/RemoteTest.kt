fea1b1eff4f238b4e6a7b8a648e4ea81fcec4734
suhas-kotagal <75802454+suhas-kotagal@users.noreply.github.com>
2021-04-16 13:53:00 +0530

    Update RemoteTest.kt

--- app/src/androidTest/java/com/logitech/integration/test/remote/RemoteTest.kt
+++ app/src/androidTest/java/com/logitech/integration/test/remote/RemoteTest.kt
@@ -1,4 +1,4 @@
-package com.logitech.integration.test.remote
+/*package com.logitech.integration.test.remote
 
 import androidx.test.filters.MediumTest
 import com.logitech.integration.test.common.rules.RemoteRule
@@ -6,7 +6,7 @@ import com.logitech.integration.test.helpers.remote.modeChangeAndAssert
 import org.junit.Rule
 import org.junit.Test
 
-/**
+*
  *  adb shell am instrument -w -r -e class 'com.logitech.integration.test.remote.RemoteTest' com.logitech.integration.test/androidx.test.runner.AndroidJUnitRunner
  *  #Run with coverage
  *
@@ -14,7 +14,7 @@ import org.junit.Test
  *  adb shell am instrument -w -r -e coverageFile /data/data/com.logitech.integration.test/coverage.ec -e coverage true -e class 'com.logitech.integration.test.AudioTest' com.logitech.integration.test.test/androidx.test.runner.AndroidJUnitRunner
  *  adb shell am instrument -w -r -e coverageFile /data/data/com.logitech.integration.test/coverage.ec -e coverage true -e class 'com.logitech.integration.test.AudioTest' com.logitech.integration.test.test/androidx.test.runner.AndroidJUnitRunner
  * START FLAGS IS: cn = ComponentInfo{com.logitech.integration.test.test/androidx.test.runner.AndroidJUnitRunner}, pf = null, flags=0, args=Bundle[{coverageFile=/data/data/com.logitech.integration.test/coverage.ec, coverage=true, class=com.logitech.integration.test.remote.RemoteTest}], watcher=com.android.commands.am.Instrument$InstrumentationWatcher@9cfdba6, connection=android.app.UiAutomationConnection@ad149e7, userId = -2, abi=null
- */
+ 
 
 class RemoteTest(){
 
@@ -28,4 +28,4 @@ class RemoteTest(){
             modeChangeAndAssert(remoteServiceHelper)
         }
     }
-}
+}*/
